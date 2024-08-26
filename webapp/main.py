04eea6625b4d8a3902029e499e08085f2e5aef60
Sleepyhead <69421538+Sleepyhead01@users.noreply.github.com>
2024-08-26 22:03:51 +0530

    vllm init

--- webapp/main.py
+++ webapp/main.py
@@ -3,21 +3,28 @@ from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import torch
 import time
+from vllm import LLM, SamplingParams
 
-# Check if CUDA is available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Model will be loaded on device: {device}")
+# choosing the large language model
+llm = LLM(model="gpt2")
 
-# Load pre-trained model and tokenizer
-model_name = 'gpt2'
-start_time = time.time()
-model = GPT2LMHeadModel.from_pretrained(model_name).to(device)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-print(f"Time taken to load model and tokenizer: {time.time() - start_time:.2f} seconds")
+# setting the parameters
+sampling_params = SamplingParams(temperature=0.8, top_p=0.90,max_tokens = 50)
 
-start_time = time.time()
-model = torch.compile(model, mode="max-autotune")
-print(f"Time taken to compile model: {time.time() - start_time:.2f} seconds")
+# # Check if CUDA is available
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(f"Model will be loaded on device: {device}")
+
+# # Load pre-trained model and tokenizer
+# model_name = 'gpt2'
+# start_time = time.time()
+# model = GPT2LMHeadModel.from_pretrained(model_name).to(device)
+# tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+# print(f"Time taken to load model and tokenizer: {time.time() - start_time:.2f} seconds")
+
+# start_time = time.time()
+# model = torch.compile(model, mode="max-autotune")
+# print(f"Time taken to compile model: {time.time() - start_time:.2f} seconds")
 
 app = FastAPI()
 
@@ -26,20 +33,17 @@ class Body(BaseModel):
 
 @app.get('/')
 def root():
-    return Response(f"<h1>A self-documenting API to interact with a GPT2 model and generate text using PyTorch (Running on: {device})</h1>")
+    return Response(f"<h1>A self-documenting API to interact with a GPT2 model and generate text using vLLM</h1>")
 
 @app.post('/generate')
 def predict(body: Body):
-    input_ids = tokenizer.encode(body.text, return_tensors='pt').to(device)
-    
-    # Measure the inference time
+    # defining our prompt
+    prompt = body.text
     start_time = time.time()
+    # generating the answer
+    answer = llm.generate(prompt,sampling_params)
 
-    # Generate text
-    with torch.no_grad():
-        output = model.generate(input_ids, max_length=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-    
+    # getting the generated text out from the answer variable
+    generated_text = answer[0].outputs[0].text
     inference_time = time.time() - start_time
-    
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     return {"generated_text": generated_text, "inference_time_seconds": inference_time}
